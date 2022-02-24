5d391c71269f831936c5429fefa0c5e6a0cb9ae0
eminetolun <eminetolun0@gmail.com>
2022-02-24 17:29:47 +0300

    Update AB testing.py

--- AB testing.py
+++ AB testing.py
@@ -1,3 +1,7 @@
+
+
+# Import Packages
+
 import itertools
 import numpy as np
 import pandas as pd
@@ -12,15 +16,15 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', 10)
 pd.set_option('display.float_format', lambda x: '%.5f' % x)
 
-## DEĞİŞKENLER
+## Variables
 
-#Impression : Reklam görüntüleme sayısı
-#Click
-#Purchase:Satın Alım
-#Earning : Kazanç
+#Impression : Number of ad views
+#Click : Click
+#Purchase: Indicates the number of products purchased after the clicked ads.
+#Earning : Earnings after purchased items
 
 
-
+# Read Excel
 trainSet = pd.read_excel(".../data.xlsx", sheet_name="Control Group")
 testSet = pd.read_excel(".../data.xlsx", sheet_name="Test Group")
 
@@ -32,18 +36,19 @@ len(trainSet)
 len(testSet)
 trainSet.columns
 ############################
-# 1. Varsayım Kontrolü
+# 1. Assumption Check
 ############################
 
-# Normallik Varsayımı
-# Varyans Homojenliği
+# Normality Assumption
+# Variance Homogeneity
 
 ############################
-# Normallik Varsayımı
+# Normality Assumption
 ############################
 
-# H0: Normal dağılım varsayımı sağlanmaktadır.
-# H1: Normal dağılım varsayımı sağlanmamaktadır.
+#H0: There is no difference between the number of products purchased. (p-value < 0.05)
+#H1: Normal distribution assumption not provided.
+
 
 test_stat, pvalue = shapiro(trainSet["Purchase"])
 print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
@@ -55,33 +60,27 @@ print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
 
 # Test Stat = 0.9589, p-value = 0.1541
 
-# İki veri seti için shapiro testi p value değeri > 0,05'ten büyük olduğu için  normal dağılımı varsayımı sağlanmaktadır. H0 reddedilemez.
-#İkinci varsayım olan varyans homojenliği varsayımı kontrol edilir.
+# Fail to reject H0. Normal Distribution .
 
 ############################
-# Varyans Homojenligi Varsayımı
+# Variance Homogeneity
 ############################
 
-# H0: Varyanslar Homojendir
-# H1: Varyanslar Homojen Değildir
-
+#H0: Variances are Homogeneous
+#H1: Variances Are Not Homogeneous
 
 test_stat, pvalue  = levene(trainSet["Purchase"], testSet["Purchase"])
 print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
 
-# H0 reddedilemez. İki örneklem arasındaki varyanslar homojendir.
-# Varsayımlar sağlandığı için bağımsız iki örneklem t testine geçilebilir.
+# Fail to reject H0. Variances are Homogeneous.
 
 
-#HİPOTEZLER
-# H0 : M1 =  M2 (İki grup ortalamaları arasında istatistiksel olarak anlamlı bir fark yoktur.)
-# H1 : M1 !=  M2 (İki grup ortalamaları arasında istatistiksel olarak anlamlı bir fark vardır.)
+# H0 : M1 =  M2 (A/B groups are similar.)
+# H1 : M1 !=  M2 (A/B groups are not similar.)
 
 test_stat , pvalue = ttest_ind(trainSet["Purchase"],testSet["Purchase"] )
 print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
 
-# pvalue > 0,05 olduğu için H0 reddedilemez. İki grup arasında istatistiksel olarak anlamlı bir fark yoktur.
-# Mevcut teklif verme türünü kullanmaya devam etmelidir.
-
+# Fail to reject H0.A/B groups are similar.
 
 
